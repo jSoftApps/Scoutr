02263fbc1b038abdf016d74151781c86513f6939
James Kienle <james@jsoftapps.com>
2015-01-12 19:35:26 -0800

    Quick DB Changes

---
+++ newstyle/footer.php
@@ -0,0 +1,20 @@
+    <!-- jQuery -->
+    <script src="assets/plugins/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="assets/plugins/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="assets/plugins/metisMenu/dist/metisMenu.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="assets/plugins/raphael/raphael-min.js"></script>
+    <script src="assets/plugins/morrisjs/morris.min.js"></script>
+    <script src="assets/js/morris-data.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="assets/js/sb-admin-2.js"></script>
+
+</body>
+
+</html>
---
+++ newstyle/header.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Dashboard - Scoutr v 2.0.0</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="assets/plugins/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Bootstrap Metro Theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+    
+    <!-- Bootstrap Metro Menu Restyle
+    <link href="assets/css/customnav.css" rel="stylesheet">  -->
+
+    <!-- MetisMenu CSS -->
+    <link href="assets/plugins/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Timeline CSS -->
+    <link href="assets/css/timeline.css" rel="stylesheet">
+
+    <!-- SB Admin CSS -->
+    <link href="assets/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="assets/plugins/morrisjs/morris.css" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Scoutr v 2.0.0</a>
+            </div>
+            <!-- /.navbar-header -->
+
+            <ul class="nav navbar-top-links navbar-right">
+              <!-- We don't really have a use for a top menu right now, so there's nothing here. TODO: Add top menu content -->
+            </ul>
+            <!-- /.navbar-top-links -->
+
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-search">
+                          <!-- Profile Section -->
+                          <h5><a href="test"><b>James Kienle</b></a></h5><br>
+                          <ul class="list-inline userlist">
+                            <li><a href=""><i class="fa fa-user"></i></a></li>
+                            <li><a href=""><i class="fa fa-users"></i></a></li>
+                            <li><a href=""><i class="fa fa-sign-out"></i></a></li>
+                          </ul>
+                        </li>
+                        <li class="active">
+                            <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
